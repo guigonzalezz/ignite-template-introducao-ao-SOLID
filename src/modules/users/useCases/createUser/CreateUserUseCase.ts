53d23cefe5d5029e097b1d90902ef02452e83910
Guilherme Camargo Gonzalez <guilherme.gonzales@fcamara.com.br>
2021-06-24 15:23:41 -0300

    desafio  finalizado

--- src/modules/users/useCases/createUser/CreateUserUseCase.ts
+++ src/modules/users/useCases/createUser/CreateUserUseCase.ts
@@ -7,10 +7,15 @@ interface IRequest {
 }
 
 class CreateUserUseCase {
-  constructor(private usersRepository: IUsersRepository) {}
+  constructor(private usersRepository: IUsersRepository) { }
 
   execute({ email, name }: IRequest): User {
-    // Complete aqui
+    const user = this.usersRepository.findByEmail(email);
+
+    if (user) {
+      throw new Error("User already exists!");
+    }
+    return this.usersRepository.create({ email, name });
   }
 }
 
--- src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
+++ src/modules/users/useCases/showUserProfile/ShowUserProfileController.ts
@@ -3,10 +3,16 @@ import { Request, Response } from "express";
 import { ShowUserProfileUseCase } from "./ShowUserProfileUseCase";
 
 class ShowUserProfileController {
-  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) {}
+  constructor(private showUserProfileUseCase: ShowUserProfileUseCase) { }
 
   handle(request: Request, response: Response): Response {
-    // Complete aqui
+    try {
+      const { user_id } = request.params;
+      const user = this.showUserProfileUseCase.execute({ user_id });
+      return response.status(201).json(user);
+    } catch (err) {
+      return response.status(404).json({ error: err.message });
+    }
   }
 }
 
